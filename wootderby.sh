af86cad9a1264dd3c7ac99c7729bc1db3eea2e40
Scott Frazier <scott.frazier@knights.ucf.edu>
2011-07-20 00:59:42 -0400

    now with more mktemp!

--- wootderby.sh
+++ wootderby.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
-curl -s http://shirt.woot.com/Derby/Default.aspx?sort=votes | grep "Tier2.*OverlayDiv" | sed "s:.*9;\(.*\)&.*:\1:" > .var.temp
+tmp=`mktemp .tmp.XXX`
+curl -s http://shirt.woot.com/Derby/Default.aspx?sort=votes | grep "Tier2.*OverlayDiv" | sed "s:.*9;\(.*\)&.*:\1:" > $tmp
 num=0
 while read line
 do
 	let "num += 1"
 	wget -q `curl -s $line | grep "lazysrc" | sed "s:.*\(http.*jpg\).*:\1:"` -O .pic${num}.jpg
-done < .var.temp
+done < $tmp
 feh -D 3 .pic[1-${num}].jpg
-rm .pic[1-${num}].jpg .var.temp
+rm .pic[1-${num}].jpg $tmp
--- wootshirt.sh
+++ wootshirt.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
-curl -s http://shirt.woot.com/Blog/Feed.ashx | grep 'href=.http://sale' | sed "s:.*href=.\(.*Detail.png\).*:\1:" > .var.temp
-day=0
+tmp=`mktemp .tmp.XXX`
+curl -s http://shirt.woot.com/Blog/Feed.ashx | grep 'href=.http://sale' | sed "s:.*href=.\(.*Detail.png\).*:\1:" > $tmp
+num=0
 while read line
 do
-	let "day += 1"
-	wget -q $line -O .shirt${day}.png;
-done < .var.temp
-feh -D 3 .shirt[1-${day}].png
-rm .shirt[1-${day}].png .var.temp
+	let "num += 1"
+	wget -q $line -O .shirt${num}.png;
+done < $tmp
+feh -D 3 .shirt[1-${num}].png
+rm .shirt[1-${num}].png $tmp
